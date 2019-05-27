652384871542858cf6b19ddfd81ef65427b3505f
Yemre <aybey77@gmail.com>
2019-05-27 14:46:57 +0300

    creation board class

---
+++ lib/board.rb
@@ -0,0 +1,20 @@
+class Board
+  @@board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def show_board
+    i = 0
+    while i < @board.length
+      puts " #{@board[i]} | #{@board[i+1]} | #{@board[i+2]}"
+      puts "---+---+---"
+      i = i+3
+    end
+  end
+
+  def update_board(num, char)
+    @board[num-1] = char
+    self.show_board
+  end
+end
