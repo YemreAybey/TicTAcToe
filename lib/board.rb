bee298e72afd9f7491b89f543651afd167bb5139
Yemre <aybey77@gmail.com>
2019-05-28 19:31:10 +0300

    Game Logic

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Board
   @@winner_points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 3, 6, 1, 4, 7, 2, 5, 8, 0, 4, 8, 6, 4, 2]
   def initialize
@@ -7,37 +9,33 @@ class Board
   def show_board
     i = 0
     while i < @board.length
-      puts " #{@board[i]} | #{@board[i+1]} | #{@board[i+2]}"
-      puts "---+---+---"
-      i = i+3
+      puts " #{@board[i]} | #{@board[i + 1]} | #{@board[i + 2]}"
+      puts '---+---+---'
+      i += 3
     end
   end
 
   def update_board(num, char)
-    @board[num-1] = char
+    @board[num - 1] = char
   end
 
-  def check_board    
+  def check_board
     @board.any? Integer
   end
 
   def check_sign(num)
-    if @board[num.to_i - 1] == num.to_i
-      true
-    else
-      false
-    end
+    @board[num.to_i - 1] == num.to_i
   end
 
   def check_winner
     i = 0
     while i < @@winner_points.length
-      if @board[@@winner_points[i]] == @board[@@winner_points[i+1]] && @board[@@winner_points[i]] == @board[@@winner_points[i+2]]
+      if @board[@@winner_points[i]] == @board[@@winner_points[i + 1]] && @board[@@winner_points[i]] == @board[@@winner_points[i + 2]]
         return true
       end
-      i = i + 3
+
+      i += 3
     end
     false
   end
 end
-
--- lib/game.rb
+++ lib/game.rb
@@ -1,143 +1,141 @@
+# frozen_string_literal: true
+
 require_relative 'board.rb'
 require_relative 'player.rb'
 
-class Game  
+class Game
   def initialize
     @new_board = Board.new
     @player1 = Player.new
     @player2 = Player.new
-    @new_board.show_board     
+    @new_board.show_board
     instructions
-    self.play
-  end  
+    play
+  end
+
   def instructions
     puts "Tictactoe game is 3X3 board game. You need to choose either X or O\n
-    Every turn the players puts the sign X or O on their turn\n if 
+    Every turn the players puts the sign X or O on their turn\n if
     any player gets conswquetive X or O then he or she wins"
   end
 
   def play
-    puts "Choose X or O"
+    puts 'Choose X or O'
     @player1.sign = gets.chomp
-    
-    while @player1.sign != "X" &&  @player1.sign  != "O"
-      puts "please choose X or O"
-      @player1.sign = gets.chomp      
+
+    while @player1.sign != 'X' && @player1.sign != 'O'
+      puts 'please choose X or O'
+      @player1.sign = gets.chomp
     end
 
-    if @player1.sign == "X"
-      @player2.sign = "O"
-    else
-      @player2.sign = "X"
-    end
-  
+    @player2.sign = if @player1.sign == 'X'
+                      'O'
+                    else
+                      'X'
+                    end
+
     while @new_board.check_board
-      puts "please choose any number from board to put #{@player1.sign}"  
+      puts "please choose any number from board to put #{@player1.sign}"
       cell = gets.chomp
 
-        until @new_board.check_sign(cell)
-          if (1..9).include?(cell.to_i)
-            puts "This number is not available. Please choose another number"
-          else
-          puts "This is a string please put number"
-        end
-          cell = gets.chomp
+      until @new_board.check_sign(cell)
+        if (1..9).include?(cell.to_i)
+          puts 'This number is not available. Please choose another number'
+        else
+          puts 'This is a string please put number'
+      end
+        cell = gets.chomp
+      end
+
+      @new_board.update_board(cell.to_i, @player1.sign)
+      @new_board.show_board
+      is_winner = check_game
+
+      if is_winner == true
+        puts 'Player1 won'
+        puts 'Would you like to play again? Type Y/N'
+        input = gets.chomp
+        until input == 'Y' || input == 'N'
+          puts 'Please enter Y or N'
+          input = gets.chomp
+      end
+        if input == 'N'
+          puts "Thanks"
+          exit 
+        else
+          Game.new
         end
 
-          @new_board.update_board(cell.to_i, @player1.sign)
-          @new_board.show_board
-          is_winner = check_game
-
-          if is_winner == true
-            puts "Player1 won"
-            puts "Would you like to play again? Type Y/N"
-            input = gets.chomp
-              until input == "Y" || input == "N"              
-              puts "Please enter Y or N"
-              input = gets.chomp
-            end                      
-            if input == "Y"            
-            @new_board = Board.new
-            play
-            else
-              puts "Thank you for playing game"
-              break
-            end
-            
-          elsif is_winner == false && !@new_board.check_board
-            puts "It is a draw"
-            puts "Would you like to play again? Type Y/N"
-            input = gets.chomp
-            until input == "Y" || input == "N"              
-              puts "Please enter Y or N"
-              input = gets.chomp
-            end          
-            if input == "Y"            
-              @new_board = Board.new
-              play              
-              else
-                puts "Thank you for playing game"
-                break
-              end            
+      elsif is_winner == false && !@new_board.check_board
+        puts 'It is a draw'
+        puts 'Would you like to play again? Type Y/N'
+        input = gets.chomp
+        until input == 'Y' || input == 'N'
+          puts 'Please enter Y or N'
+          input = gets.chomp
+        end
+        if input == 'Y'
+          @new_board = Board.new
+          play
+        else
+          puts 'Thank you for playing game'
+          return
           end
-  
-      puts "please choose any number from board to put #{@player2.sign}"  
+      end
+
+      puts "please choose any number from board to put #{@player2.sign}"
       cell = gets.chomp
       until @new_board.check_sign(cell)
         if (1..9).include?(cell.to_i)
-          puts "This number is not available. Please choose another number"
+          puts 'This number is not available. Please choose another number'
         else
-          puts "This is a string please put number"
+          puts 'This is a string please put number'
         end
         cell = gets.chomp
-      end 
+      end
 
       @new_board.update_board(cell.to_i, @player2.sign)
       @new_board.show_board
       is_winner = check_game
 
-          if is_winner == true
-            puts "Player1 won"
-            puts "Would you like to play again? Type Y/N"
-            input = gets.chomp
-              until input == "Y" || input == "N"              
-              puts "Please enter Y or N"
-              input = gets.chomp
-            end                      
-            if input == "Y"            
-            @new_board = Board.new
-            play
-            else
-              puts "Thank you for playing game"
-              break
-            end
-            
-          elsif is_winner == false && !@new_board.check_board
-            puts "It is a draw"
-            puts "Would you like to play again? Type Y/N"
-            input = gets.chomp
-            until input == "Y" || input == "N"              
-              puts "Please enter Y or N"
-              input = gets.chomp
-            end          
-            if input == "Y"            
-              @new_board = Board.new
-              play              
-              else
-                puts "Thank you for playing game"
-                break
-              end            
-          end
+      if is_winner == true
+        puts 'Player2 won'
+        puts 'Would you like to play again? Type Y/N'
+        input = gets.chomp
+        until input == 'Y' || input == 'N'
+          puts 'Please enter Y or N'
+          input = gets.chomp
+      end
+      exit("Thank you for playing") if input == 'N'
+      return Game.new
 
-      end    
+      elsif is_winner == false && !@new_board.check_board
+        puts 'It is a draw'
+        puts 'Would you like to play again? Type Y/N'
+        input = gets.chomp
+        until input == 'Y' || input == 'N'
+          puts 'Please enter Y or N'
+          input = gets.chomp
+        end
+        if input == 'Y'
+          @new_board = Board.new
+          play
+        else
+          puts 'Thank you for playing game'
+          return
+          end
+      end
+
+      end
   end
+
   def check_game
-      winner
+    winner
   end
+
   def winner
     @new_board.check_winner
   end
 end
 
 game_1 = Game.new
-
