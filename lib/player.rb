2a95aa4754212d77f9e882da5afa2bc8f8bf196f
NeerNim <neernimt@gmail.com>
2019-05-30 15:07:10 +0200

    create method for player to handle move

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'ui.rb'
+require_relative 'board.rb'
+
 class Player
   attr_accessor :sign
   def initialize
     self.sign = nil
+  end
+
+  def assign_sign(sign)
+    self.sign = sign.choose_sign
+  end
+
+  def make_move(board, cell)
+    board.update_board(cell, sign)
   end
 end
