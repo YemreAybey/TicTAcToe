be6f5298537d7322bb731622e5f809a55476bcaa
NeerNim <neernimt@gmail.com>
2019-05-30 15:03:20 +0200

    create instance of game element in main

--- bin/main.rb
+++ bin/main.rb
@@ -3,4 +3,18 @@ require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
 
-Game.new
+
+board = Board.new
+player1 = Player.new
+player2 = Player.new
+ui = UI.new
+
+answer = Game.new(board, player1, player2, ui)
+until answer.continue == "N"
+  board = Board.new
+  player1 = Player.new
+  player2 = Player.new
+  ui = UI.new
+  answer = Game.new(board, player1, player2, ui)
+end
+
