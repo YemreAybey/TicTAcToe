32d2972c361aaf2a13b78165558ffb590f7907e8
NeerNim <neernimt@gmail.com>
2019-05-31 13:35:41 +0200

    create new module file

---
+++ lib/module.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board.rb'
+require_relative '../lib/player.rb'
+require_relative '../lib/game.rb'
+require_relative '../lib/module.rb'
+
+include UI
+
+module BoardLogic
+  def continue_until_no(answer)
+    until answer.continue == 'N'
+      value = choose_sign
+      value2 = decide_other_sign(value)
+      board = Board.new
+      player1 = Player.new(value)
+      player2 = Player.new(value2)
+      answer = Game.new(board, player1, player2)
+    end
+  end
+end
